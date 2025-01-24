e3818551f07d8b299c3c53a2d9128155852b7198
suman2025-kiit <dr.suman.majumder.postdoc@gmail.com>
2025-01-24 17:28:15 +0530

    Add files via upload

---
+++ sm/solution/csim/build/obj/no_os_spi.d
@@ -0,0 +1,3 @@
+obj/no_os_spi.o: ../../../../no_os_spi.c \
+  C:/Xilinx/Vitis_HLS/2021.2/common/technology/autopilot\etc/autopilot_ssdm_op.h \
+  ../../../../no_os_spi.h
---
+++ sm/solution/csim/build/obj/xil_cache.d
@@ -0,0 +1,3 @@
+obj/xil_cache.o: ../../../../xil_cache.c \
+  C:/Xilinx/Vitis_HLS/2021.2/common/technology/autopilot\etc/autopilot_ssdm_op.h \
+  ../../../../xil_cache.h
